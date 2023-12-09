3282b2f6327b495d67070ac445ca88e58c159527
EwoutMaertens <maertens.ewout@gmail.com>
2023-12-10 00:09:05 +0100

    Adding basic explanation of usage to wsgi script

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,8 @@ import os
 
 from Lesson.lesson.application.app import create_app
 
+# This script will be automatically selected when running flask
+# Usage: 'FLASK_CONFIG="development"  flask run'
+# Then surf to 'http://127.0.0.1:5000/lesson_groups' to see results
+
 app = create_app(os.environ["FLASK_CONFIG"])
